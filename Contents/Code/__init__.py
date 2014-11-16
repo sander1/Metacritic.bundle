2379311f118b8e2a15e42ef83cb1082b9f10e499
Sander <sander@plex.tv>
2014-11-16 19:58:24 +0100

    first commit

---
+++ Contents/Code/__init__.py
@@ -0,0 +1,46 @@
+API_URL = 'https://byroredux-metacritic.p.mashape.com/search/movie'
+
+####################################################################################################
+def Start():
+
+	HTTP.Headers['User-Agent'] = 'Plex Media Server/0.9'
+	HTTP.Headers['X-Mashape-Key'] = '7o60PGcAVLmshjnGCFnhgZnkmFHlp1KDMMHjsn8ROsKmhryyFT'
+
+####################################################################################################
+class Metacritic(Agent.Movies):
+
+	name = 'Metacritic'
+	languages = [Locale.Language.NoLanguage]
+	primary_provider = False
+	contributes_to = [
+		'com.plexapp.agents.imdb',
+		'com.plexapp.agents.themoviedb'
+	]
+
+	def search(self, results, media, lang):
+
+		results.Append(MetadataSearchResult(
+			id = '%s|%s' % (media.primary_metadata.title, media.primary_metadata.year),
+			score = 100
+		))
+
+	def update(self, metadata, media, lang):
+
+		(title, year) = metadata.id.rsplit('|', 1)
+
+		post_values = {
+			'max_pages': '1',
+			'retry': '4',
+			'title': title,
+			'year_from': year,
+			'year_to': str(int(year) + 1) if year != '' else ''
+		}
+
+		try:
+			json_obj = JSON.ObjectFromURL(API_URL, values=post_values)
+		except:
+			Log('*** Error retrieving data for "%s" ***' % (title))
+			return None
+
+		if 'results' in json_obj and len(json_obj['results']) > 0:
+			metadata.rating = float(json_obj['results'][0]['score'])/10
